970fe5a35413f36a5db4f1f4524bde6fecfdc1f7
JABC <jesuscruzba20@gmail.com>
2022-09-23 01:06:35 -0500

    Tarea_5

--- Tarea5_ordenamiento_seleccion/Ord_Selec/src/ord_selec/Ord_Selec.java
+++ Tarea5_ordenamiento_seleccion/Ord_Selec/src/ord_selec/Ord_Selec.java
@@ -7,7 +7,7 @@ package ord_selec;
 
 public class Ord_Selec {
     
-    public static void main(int[] arr) {
+    public static void main(int[] arr){
         int pivote = 0;
         int temp = 0;
         int aux = 0;
@@ -16,7 +16,7 @@ public class Ord_Selec {
         for (pivote = 0; pivote < arr.length; pivote++){
             min = pivote;
             for (aux = pivote + 1; aux < arr.length; aux++){
-                if(arr[aux] < arr[min]) {
+                if(arr[aux] < arr[min]){
                     min = aux;
                 }
             }
@@ -26,9 +26,9 @@ public class Ord_Selec {
         }
     }
 
-    public void impr(int[] arr){
-        for(int h = 0; h < arr.length; h++){
-            System.out.print(arr[h] + "\\t");
+    public void imp(int[] arr){
+        for(int j = 0; j < arr.length; j++){
+            System.out.print(arr[j] + "\\t");
         }    
     }
 }
